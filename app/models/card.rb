c3add3790563d1add43c706a29bae2c6cf88aa6a
Elaya Sison <asison13@gmail.com>
2016-01-28 14:35:58 -0800

    Added new references

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ActiveRecord::Base
 
-  # belongs_to :turn
+  belongs_to :turn
 
   # RANKS = %w(2 3 4 5 6 7 8 9 10 J Q K A)
   # SUITS = %w(Spade Heart Club Diamond)
--- db/migrate/20160128031008_add_cards.rb
+++ db/migrate/20160128031008_add_cards.rb
@@ -1,6 +1,7 @@
 class AddCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
+      t.references :turn, index: true
       t.string :rank
       t.string :suit
       t.string :rule
